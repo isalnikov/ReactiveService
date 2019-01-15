403f3d325413cd8222871a2888d8e88dc2990c0c
i.salnikov <igor.salnikov@stoloto.ru>
2019-01-15 19:16:07 +0300

    https://cloud.spring.io/spring-cloud-function/

--- ReactiveService/src/main/java/com/isalnikov/reactiveservice/GreetingHandler.java
+++ ReactiveService/src/main/java/com/isalnikov/reactiveservice/GreetingHandler.java
@@ -1,18 +1,28 @@
 
 package com.isalnikov.reactiveservice;
+import java.util.function.Function;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 
 import reactor.core.publisher.Mono;
 
 @Component
 public class GreetingHandler {
+    
+   @Autowired 
+   Function<Flux<String>, Flux<String>> uppercase;
 
 	public Mono<ServerResponse> hello(ServerRequest request) {
+            
+       Mono<String> mono = uppercase.apply(Flux.just("Hello, Spring!")).next();
+            
 		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
-			.body(BodyInserters.fromObject("Hello, Spring!"));
+			//.body(BodyInserters.fromObject( "Hello, Spring!"));
+			.body(BodyInserters.fromPublisher(mono,String.class));
 	}
 }
--- ReactiveService/src/main/java/com/isalnikov/reactiveservice/GreetingRouter.java
+++ ReactiveService/src/main/java/com/isalnikov/reactiveservice/GreetingRouter.java
@@ -1,6 +1,6 @@
-
 package com.isalnikov.reactiveservice;
 
+import java.util.function.Function;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -8,14 +8,21 @@ import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 
 @Configuration
 public class GreetingRouter {
 
-	@Bean
-	public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+    @Bean
+    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
 
-		return RouterFunctions
-			.route(RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), greetingHandler::hello);
-	}
+        return RouterFunctions
+                .route(RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), greetingHandler::hello);
+    }
+
+    @Bean
+    public Function<Flux<String>, Flux<String>> uppercase() {
+        return flux -> flux.map(value -> value.toUpperCase());
+    }
+
 }
